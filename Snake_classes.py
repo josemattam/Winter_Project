680bd08683fe3e0310255114845a51498f705c5e
josemattam <josesebanm@gmail.com>
2020-12-15 21:30:04 +0300

    changed snake to a deque, added to the world

--- Snake_classes.py
+++ Snake_classes.py
@@ -2,9 +2,12 @@ from dataclasses import dataclass
 import random
 import pygame
 import tkinder as tk
+import numpy as np
+from collections import deque
+
 
 @dataclass
-class block():
+class Block():
     
     # x and y coordinates:
     xloc: int
@@ -21,31 +24,31 @@ class block():
         self.xloc = _xloc
         self.yloc = _yloc
         self.block_type = _block_type
+
         
     
 @dataclass
-class snake():
+class Snake():
     
     # head location
     head_x: int
     head_y: int 
     
     # array list of snake blocks
-    snake_blocks: []
-    
-    # snake length
-    length: int
-    
+    snake_blocks: deque[Block]
+        
     # length of the square grid map
     grid_length: int
     
     # direction the snake is facing
     direction: str
     
+    # snake length
+    length: int
     
     # constructor: will create the snake and add it to the world map
     # requirements: either no parameters or all included parameters
-    def __init__(self, head_x = 3, head_y = 8, grid_length = 10):
+    def __init__(self, head_x = 3, head_y = 8, grid_length = 10, direction = "east"):
         
         
         create_snake()
@@ -57,6 +60,8 @@ class snake():
     # TODO: make the body spawn in a random direction
     def create_snake(self):
         
+        # not sure if we need to initialize snake_blocks
+        
         # add the head to the snake_block list
         snake_blocks.append(block(head_x, head_y, 1))
         
@@ -64,7 +69,8 @@ class snake():
         snake_blocks.append(block(head_x - 1, head_y, 1))
         snake_blocks.append(block(head_x - 2, head_y, 1))
 
-        direction = "east"
+        direction = direction
+        length = 3
         return
     
     
@@ -72,6 +78,57 @@ class snake():
         
         
         
+@dataclass
+class World():
+    
+    # length of the square grid map
+    grid_length: int
+       
+    # world grid
+    world_grid: [int][int]
+    
+    # the player's snake
+    snake: Snake
+    
+    def __init__(self, grid_length = 10):
+        
+        # initialize the world grid to a 2d array of zeroes
+        world_grid = np.zeros((grid_length, grid_length))
+        
+        # initialze the snake
+        snake()
+        pass
+    
+    # move the snake in a new or same direction based on the input direction given    
+    def move_snake(self, _direction):
+        
+        # if the given direction is the same as the current direction,
+        # then move in the same direction
+        if (snake.direction == _direction)
+            move(snake.direction)           
+            
+        # if the given direction is the opposite direction the snake faces,
+        # then move in the same direction the snake was moving
+        else if (snake.direction == "north" && _direction == "south" ||
+                 snake.direction == "south" && _direction == "north" ||
+                 snake.direction == "east" && _direction == "west" ||
+                 snake.direction == "west" && _direction =+ "east")
+            move(snake.direction)
+
+        else:
+            # move in the valid new direction
+            move(_direction)
+
+        return  
+        
+    # private helper function that moves the snake in the grid and sets its new direction
+    def __move(self, _direction):
+        
+        
+        
+        pass
+    
+    #add to the snake length when it eats
     
 """        
     # returns the list of snake blocks
